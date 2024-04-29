a5991e4b505e3a3de19bb2e20fb0e329bbfe0db2
Cary Huang <cary.huang@highgo.ca>
2024-04-29 12:18:17 -0700

    adjusted regress

--- src/test/regress/sql/select_parallel.sql
+++ src/test/regress/sql/select_parallel.sql
@@ -497,16 +497,16 @@ SELECT 1 FROM tenk1_vw_sec
 
 -- test parallel tid range scan
 EXPLAIN (COSTS OFF)
-SELECT stringu1 FROM tenk1 WHERE ctid > '(0,1)' LIMIT 1;
-SELECT stringu1 FROM tenk1 WHERE ctid > '(0,1)' LIMIT 1;
+SELECT ctid FROM tenk1 WHERE ctid > '(0,1)' LIMIT 1;
+SELECT ctid FROM tenk1 WHERE ctid > '(0,1)' LIMIT 1;
 
 EXPLAIN (COSTS OFF)
-SELECT stringu1 FROM tenk1 WHERE ctid < '(400,1)' LIMIT 1;
-SELECT stringu1 FROM tenk1 WHERE ctid < '(400,1)' LIMIT 1;
+SELECT ctid FROM tenk1 WHERE ctid < '(400,1)' LIMIT 1;
+SELECT ctid FROM tenk1 WHERE ctid < '(400,1)' LIMIT 1;
 
 EXPLAIN (COSTS OFF)
-SELECT stringu1 FROM tenk1 WHERE ctid >= '(0,1)' AND ctid <= '(400,1)' LIMIT 1;
-SELECT stringu1 FROM tenk1 WHERE ctid >= '(0,1)' AND ctid <= '(400,1)' LIMIT 1;
+SELECT ctid FROM tenk1 WHERE ctid >= '(0,1)' AND ctid <= '(400,1)' LIMIT 1;
+SELECT ctid FROM tenk1 WHERE ctid >= '(0,1)' AND ctid <= '(400,1)' LIMIT 1;
 
 EXPLAIN (COSTS OFF)
 SELECT t.ctid,t2.c FROM tenk1 t,
