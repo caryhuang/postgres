78c35e76d7ee53574bf1e4b8323a147aee5eadf1
Cary Huang <cary.huang@highgo.ca>
2024-05-14 11:26:00 -0700

    revert parallel ctid rangescan tests

--- src/test/regress/sql/select_parallel.sql
+++ src/test/regress/sql/select_parallel.sql
@@ -495,19 +495,4 @@ EXPLAIN (COSTS OFF)
 SELECT 1 FROM tenk1_vw_sec
   WHERE (SELECT sum(f1) FROM int4_tbl WHERE f1 < unique1) < 100;
 
--- test parallel tid range scan
-EXPLAIN (COSTS OFF)
-SELECT ctid FROM tenk1 WHERE ctid > '(0,1)' LIMIT 1;
-
-EXPLAIN (COSTS OFF)
-SELECT ctid FROM tenk1 WHERE ctid < '(400,1)' LIMIT 1;
-
-EXPLAIN (COSTS OFF)
-SELECT ctid FROM tenk1 WHERE ctid >= '(0,1)' AND ctid <= '(400,1)' LIMIT 1;
-
-EXPLAIN (COSTS OFF)
-SELECT t.ctid,t2.c FROM tenk1 t,
-LATERAL (SELECT count(*) c FROM tenk1 t2 WHERE t2.ctid <= t.ctid) t2
-WHERE t.ctid < '(1,0)' LIMIT 1;
-
 rollback;
